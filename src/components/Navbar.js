ece32c54a5650748b6aa00a81852f0af2170d6a6
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-11-06 11:48:51 -0700

    styled finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,31 @@
 import React from "react";
+import logo from "../images/logo.svg";
+import moduleName from "../style/navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="">
-      <div className="navWrapper">
-        <div className="logo"></div>
+      <div className="logo">
+        <a href="#home">
+          <img src={logo} alt="logo" />
+        </a>
+      </div>
 
-        <div className="navigation">
-          <ul>
-            <a href="">
-              <li>Home</li>
-            </a>
-            <a href="">
-              <li>Shopify</li>
-            </a>
-            <a href="">
-              <li>About</li>
-            </a>
-            <a href="">
-              <li>Contact</li>
-            </a>
-          </ul>
-        </div>
+      <div className="navigation">
+        <ul>
+          <a href="#home">
+            <li>Home</li>
+          </a>
+          <a href="#shopify">
+            <li>Shopify</li>
+          </a>
+          <a href="#about">
+            <li>About</li>
+          </a>
+          <a href="#contact">
+            <li>Contact</li>
+          </a>
+        </ul>
       </div>
     </nav>
   );
