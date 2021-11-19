1315d0a0648526b83d901d4596618417de94d3af
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-11-18 19:34:01 -0800

    coverted navigation into styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,58 @@
 import React from "react";
 import logo from "../images/logo.svg";
-import moduleName from "../style/navbar.css";
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
-    <nav className="">
-      <div className="logo">
-        <a href="#home">
-          <img src={logo} alt="logo" />
-        </a>
-      </div>
+    <>
+      <NavbarContainer>
+        <LogoDiv className="logo">
+          <Logo src={logo} alt="logo" />
+        </LogoDiv>
 
-      <div className="navigation">
-        <ul>
-          <a href="#home">
-            <li>Home</li>
-          </a>
-          <a href="#shopify">
-            <li>Shopify</li>
-          </a>
-          <a href="#about">
-            <li>About</li>
-          </a>
-          <a href="#contact">
-            <li>Contact</li>
-          </a>
-        </ul>
-      </div>
-    </nav>
+        <Navigation>
+          <Ul>
+            <Li>Home</Li>
+            <Li>Shopify</Li>
+            <Li>About</Li>
+            <Li>Contact</Li>
+          </Ul>
+        </Navigation>
+      </NavbarContainer>
+    </>
   );
 };
 
 export default Navbar;
+
+const NavbarContainer = styled.nav`
+  max-width: 1440px;
+  margin: 0 auto;
+  font-family: "Roboto", sans-serif;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #c3ccd9;
+`;
+
+const LogoDiv = styled.div``;
+
+const Logo = styled.img``;
+
+const Navigation = styled.div``;
+
+const Ul = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.13rem;
+  line-height: 21px;
+`;
+
+const Li = styled.li`
+  margin-right: 2em;
+`;
