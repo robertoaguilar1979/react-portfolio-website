0228f7e77810b999947d8671569182cd6729c15c
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-11-26 23:03:40 -0800

    adding styles to hamburger menu

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -11,3 +11,17 @@ function hamburger() {
 }
 
 export default hamburger;
+
+const Container = styled.div`
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+
+  HamburgerStripe {
+    width: 2rem;
+    height: 0.25rem;
+    border-radius: 10px;
+  }
+`;
